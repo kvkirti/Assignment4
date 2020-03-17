632a33e3191f4ba9b6bf43ef2913d777a2262fb4
kvkirti <kv.kirti.iitd@gmail.com>
2020-03-17 14:39:26 +0530

    insert

--- HashMap.java
+++ HashMap.java
@@ -3,15 +3,42 @@ import java.util.Vector;
 
 public class HashMap<V> implements HashMapInterface<V> {
 
+	public Array<V> hashTable;
+	int occupied;
+	V deleted;
+
 	public HashMap(int size) {
-		// write your code here
+		hashTable = new Array<V>(size);
+		this.size = size;
+		occupied = 0;
 	}
 
 	public V put(String key, V value){
+		int index = 0;
+		if(search(key)==-1){
+			index = hashFunction(key);
+			while(hashTable[index]!=null && hashTable[index] != deleted){
+				index = (index+1) % hashTable.length;
+			}
+			hashTable[index] = value;
+			occupied++;
+		}
+		else{
+			V oldValue = hashTable[search(key)];
+			hashTable[search(key)] = value;
+			return oldvalue;
+		}
 		// write your code here
 		return null;
 	}
 
+	public int hashFunction(String key){
+
+	}
+
+	public int search(String key){
+
+	}
 	public V get(String key){
 		// write your code here
 		return null;
